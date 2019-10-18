0f0966af03412e0dc013a984162b162e5eb0ca6c
idir <idirall22@gmail.com>
2019-10-18 14:19:17 +0100

    added provider interface

---
+++ providers.go
@@ -0,0 +1,16 @@
+package post
+
+import (
+	"context"
+
+	"github.com/idirall22/post/models"
+)
+
+// Provider interface
+type Provider interface {
+	New(context.Context, string, []*string, int64, int64) (*models.Post, error)
+	Get(context.Context, int64) (*models.Post, error)
+	List(context.Context, int, int) ([]*models.Post, error)
+	Update(context.Context, string, []*string, int64, int64) (*models.Post, error)
+	Delete(context.Context, int64, int64) error
+}
