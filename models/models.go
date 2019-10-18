45b198092e677906e6e01b9ca76d69f8bd020986
idir <idirall22@gmail.com>
2019-10-18 13:56:52 +0100

    added post model

---
+++ models/models.go
@@ -0,0 +1,19 @@
+package post
+
+import (
+	"time"
+
+	"github.com/idirall22/comment/models"
+)
+
+// Post structure
+type Post struct {
+	ID        int64             `json:"id"`
+	Content   string            `json:"content"`
+	MediaURLs []string          `json:"media_urls"`
+	UserID    int64             `json:"user_id"`
+	GroupID   int64             `json:"group_id"`
+	Comments  []*models.Comment `json:"comments"`
+	CreatedAt time.Time         `json:"created_at"`
+	DeletedAt *time.Time        `json:"deleted_at"`
+}
