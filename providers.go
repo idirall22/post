72e6bd7ef62f356ab75f9d80c7ef5b29490dee55
idir <idirall22@gmail.com>
2019-10-19 09:56:10 +0100

    update provider interface

--- providers.go
+++ providers.go
@@ -8,9 +8,9 @@ import (
 
 // Provider interface
 type Provider interface {
-	New(context.Context, string, []*string, int64, int64) (*models.Post, error)
-	Get(context.Context, int64) (*models.Post, error)
-	List(context.Context, int, int) ([]*models.Post, error)
-	Update(context.Context, string, []*string, int64, int64) (*models.Post, error)
+	New(context.Context, string, []string, int64, int64) (*models.Post, error)
+	Get(context.Context, int64, int64, int64) (*models.Post, error)
+	List(context.Context, int64, int, int) ([]*models.Post, error)
+	Update(context.Context, string, []string, int64, int64) (*models.Post, error)
 	Delete(context.Context, int64, int64) error
 }
