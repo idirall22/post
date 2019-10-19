7497dc243d91941d97d0cb27eff3c9634c50d26a
idir <idirall22@gmail.com>
2019-10-19 18:05:16 +0100

    added errors for post package

--- errors.go
+++ errors.go
@@ -1,6 +1,11 @@
 package post
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+
+	pr "github.com/idirall22/post/providers/postgres"
+)
 
 var (
 	// ErrorForm when post form is not valid
@@ -9,3 +14,43 @@ var (
 	// ErrorID when id is < 1
 	ErrorID = errors.New("Post ID not exists")
 )
+
+func parseError(err error) (string, int) {
+
+	message := ""
+	code := http.StatusBadRequest
+
+	switch err {
+
+	case ErrorForm:
+		message = ErrorForm.Error()
+		break
+
+	case ErrorForm:
+		message = ErrorForm.Error()
+		break
+
+	case pr.ErrorForeignKey:
+		message = "Data not valid"
+
+		code = http.StatusConflict
+		break
+
+	case pr.ErrorNoRow:
+		message = pr.ErrorNoRow.Error()
+		code = http.StatusNotFound
+		break
+
+	case pr.ErrorUnique:
+		message = pr.ErrorUnique.Error()
+		code = http.StatusConflict
+		break
+
+	case pr.ErrorServer:
+		message = pr.ErrorServer.Error()
+		code = http.StatusInternalServerError
+		break
+	}
+
+	return message, code
+}
