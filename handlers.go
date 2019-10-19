77fe4a72b3d54cd5dfce33ff86f4b03a3945fc28
idir <idirall22@gmail.com>
2019-10-19 10:34:13 +0100

    added posts handlers endpoints

---
+++ handlers.go
@@ -0,0 +1,28 @@
+package post
+
+import "net/http"
+
+// AddPostHandler add a post
+func AddPostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// ListPostsHandler list posts
+func ListPostsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GetPostHandler get a post
+func GetPostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// UpdatePostHandler update a post
+func UpdatePostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletePostHandler delete a post
+func DeletePostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
