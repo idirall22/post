3a8e6795cde16bd84d81204f9f57346a72ee489f
idir <idirall22@gmail.com>
2019-10-29 16:47:37 +0100

    Added broker to service

--- service.go
+++ service.go
@@ -3,15 +3,20 @@ package post
 import (
 	"database/sql"
 
+	"github.com/idirall22/post/brokers/memory"
 	pr "github.com/idirall22/post/providers/postgres"
 )
 
 // Service structure
 type Service struct {
 	provider Provider
+	broker   Broker
 }
 
 // NewService create a new  post service
 func NewService(db *sql.DB, tableName string) *Service {
-	return &Service{provider: &pr.PostgresProvider{DB: db, TableName: tableName}}
+	return &Service{
+		provider: &pr.PostgresProvider{DB: db, TableName: tableName},
+		broker:   &memory.Memory{},
+	}
 }
